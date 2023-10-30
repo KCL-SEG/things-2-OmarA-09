5e0a42ac617a9bd64abf8b44f7d95fc745259475
Omar Ahmad <omar.1.ahmad@kcl.ac.uk>
2023-10-30 09:40:58 +0000

    Define things form and display in home view

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing  # Import the Thing model from your app's models.py
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+
+        widgets = {
+            'description': forms.Textarea(), 
+            'quantity': forms.NumberInput(), 
+        }
--- things/views.py
+++ things/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
+    form = ThingForm()
+    # template 'form' which we assign to object :form
     return render(request, 'home.html', {'form': form})
